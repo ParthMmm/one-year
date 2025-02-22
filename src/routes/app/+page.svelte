b0df9b52181ab945a95dac0a0d7287ed4725cc9a
Parth Mangrola <parthmangrola@gmail.com>
2025-02-22 11:36:14 -0800

    add time

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
+	import { SvelteDate } from 'svelte/reactivity';
 	import Canvas from '../../lib/components/Canvas.svelte';
+
+	let date = new SvelteDate();
+
+	const pad = (n) => (n < 10 ? '0' + n : n);
+
+	$effect(() => {
+		const interval = setInterval(() => {
+			date.setTime(Date.now());
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
-<Canvas />
+<div>
+	<p class="pb-4 text-neutral-400 tabular-nums">
+		{date.getHours()}:{pad(date.getMinutes())}:{pad(date.getSeconds())}
+	</p>
+
+	<Canvas />
+</div>
