114a40064cbb0177ae5be9af4e1888e89fe58a50
Parth Mangrola <parthmangrola@gmail.com>
2025-02-21 22:23:01 -0800

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class=" h-screen bg-stone-700">
+	<div class="container mx-auto h-full">
+		<div class="flex h-full items-center justify-center">
+			{@render children()}
+		</div>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { goto } from '$app/navigation';
+	import Button from '../lib/components/Button.svelte';
+</script>
+
+<div class="flex flex-col items-center justify-center gap-4">
+	<h1 class="text-2xl font-bold text-white">Habit Tracker</h1>
+	<Button class="border border-neutral-400 text-white" onclick={() => goto('/app')}>Begin</Button>
+</div>
