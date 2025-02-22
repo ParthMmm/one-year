10f357777478d3b0c9beb07198d8b9f3919d76f3
Parth Mangrola <parthmangrola@gmail.com>
2025-02-22 12:24:32 -0800

    fix styles

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+	import { type Snippet } from 'svelte';
 	import { cn } from '../../utils';
-	let { className, children, onclick } = $props();
+	let { styles, children, onclick } = $props<{
+		styles?: string;
+		children: Snippet;
+		onclick?: () => void;
+	}>();
 </script>
 
 <button
 	class={cn(
 		'inline-flex h-8 items-center justify-center  rounded-md px-4 py-2 font-medium transition-colors',
-		className
+		styles
 	)}
 	{onclick}
 >
