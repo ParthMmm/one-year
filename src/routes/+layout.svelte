6b10e8e5d80715ca175a6ea1bee60e3d538b1552
Parth Mangrola <parthmangrola@gmail.com>
2025-02-22 18:26:11 -0800

    update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,12 @@
 	import { Tooltip } from 'bits-ui';
 </script>
 
-<Tooltip.Provider skipDelayDuration={250}>
+<Tooltip.Provider skipDelayDuration={500} delayDuration={500}>
 	<div class=" h-screen bg-stone-700">
-		<div class="container mx-auto h-full">
-			<div class="flex h-full items-center justify-center">
-				{@render children()}
-			</div>
+		<!-- <div class="container mx-auto h-full"> -->
+		<div class="flex h-full items-center justify-center">
+			{@render children()}
 		</div>
+		<!-- </div> -->
 	</div>
 </Tooltip.Provider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,38 @@
-<script>
+<script lang="ts">
+	import { SvelteDate } from 'svelte/reactivity';
+	import Grid from '$lib/components/Grid.svelte';
+	import Button from '$lib/components/ui/Button.svelte';
+	import { resetDates } from '$lib/state.svelte';
+	let date = new SvelteDate();
+
+	const pad = (n: number) => (n < 10 ? '0' + n : n);
+
+	$effect(() => {
+		const interval = setInterval(() => {
+			date.setTime(Date.now());
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
-<div class="flex flex-col items-center justify-center gap-4">
-	<h1 class="text-4xl font-bold text-white">One Year</h1>
-	<a
-		class="focus-visble:outline-offset-4 inline-flex h-8 items-center justify-center rounded-lg border-2 border-stone-400 p-4 font-medium text-white transition-colors focus-visible:outline focus-visible:outline-green-500"
-		href="/app">Begin</a
-	>
+<svelte:head>
+	<title>One Year</title>
+	<meta name="description" content="One year" />
+</svelte:head>
+
+<div>
+	<h1 class="font-mono text-2xl font-black text-white">One Year</h1>
+	<div class="flex flex-row justify-between pb-1 align-middle">
+		<span class="font-mono text-neutral-300 tabular-nums">
+			{date.getHours()}:{pad(date.getMinutes())}:{pad(date.getSeconds())}
+		</span>
+
+		<Button styles="text-neutral-300 text-sm  px-0 hover:text-green-500" onclick={resetDates}
+			>Reset</Button
+		>
+	</div>
+	<Grid />
 </div>
